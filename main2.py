9cfe78c27e59590e9391fb5221a8ca59c5e7603e
stellarore <none@none.none>
2023-11-04 23:19:29 -0400

    summing points initial histogram graphs

--- main2.py
+++ main2.py
@@ -1,12 +1,17 @@
 import config
 
 import itertools
+from yahoofantasy import Context
+# import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
+import math
+import time
 
 # configuration
 league_position_in_yahoo_account = 0
 week = 1
 
-from yahoofantasy import Context, League
 
 ctx = Context(client_id=config.yahoo_client_id, client_secret=config.yahoo_client_secret, refresh_token=config.refresh_token)
 leagues = ctx.get_leagues('nfl', 2023)
@@ -19,15 +24,28 @@ league = next(lg for lg in leagues if lg.id == leagues[league_position_in_yahoo_
 # for team in league.teams():
 #     print(f"Team Name: {team.name}\tManager: {team.manager.nickname}")
 
-#TODO: pull from Yahoo API
-league_positions = {'QB': 2, 'WR': 3, 'RB': 3, 'TE': 2, 'W/R/T': 2, 'K': 1, 'DEF': 1} # , 'BN': 8, 'IR': 1
+# TODO: pull from Yahoo API
+league_positions = {'QB': 2, 'WR': 3, 'RB': 3, 'TE': 2, 'W/R/T': 2, 'K': 1, 'DEF': 1}  # , 'BN': 8, 'IR': 1
 
+start = time.perf_counter()
 rosters = []
 
+def no_dupe_checker(flex, wr, rb, te):
+    for rb_dup in rb:
+        if flex['name'] == rb_dup['name']:
+            return False
+    for wr_dup in wr:
+        if flex['name'] == wr_dup['name']:
+            return False
+    for te_dup in te:
+        if flex['name'] == te_dup['name']:
+            return False
+    return True
 
+print("Loading Rosters")
 for team in league.teams():
     name = team.name
-    print(name)
+    # print(name)
     roster = team.roster(week-1)
     lineup = []
     for player in roster.players:
@@ -38,11 +56,11 @@ for team in league.teams():
     rosters.append({'name': name,
                    'lineup': lineup})
 
-
-# Create roster possiblities
+print("Listing valid positions")
+# Create roster possibilities
 league_possibilities = []
 for roster in rosters:
-    print(f'~~{roster["name"]}~~')
+    # print(f'~~{roster["name"]}~~')
     roster_possibilities = []
     for position in league_positions:
         # print(position)
@@ -53,32 +71,81 @@ for roster in rosters:
                 if player['position'] in ['WR', 'RB', 'TE']:
                     position_possibilities.append({'name': player['name'], 'points': player['points']})
             elif player['position'] == position:
-                position_possibilities.append({'name':player['name'], 'points':player['points']})
-        print('poss',position_possibilities)
-        roster_possibilities.append({'position':position, 'possibilities':position_possibilities})
+                position_possibilities.append({'name': player['name'], 'points': player['points']})
+        roster_possibilities.append({'position': position, 'possibilities': position_possibilities})
     league_possibilities.append({'manager': roster['name'], 'possibilities': roster_possibilities})
 
-# Create starting roster combinations
+# Create starting roster combinations,
+# Edit lists of combinations
+        # Remove invalid combinations where players are in multiple slots (ex. WR and W/R/T)
+        # Remove duplicate combinations where players are swapped slots (ex. WR in one combi and W/R/T in another)
+# Calculate all possible scores
+
 # combinations = list(itertools.combinations(my_list, 2)) (list, choose)
 
+print("Making roster combinations, calculating scores")
+league_combinations = []
 for manager in league_possibilities:
+    position_combinations = {}
     for position_possibilities in manager['possibilities']:
         position_slots = league_positions[position_possibilities['position']]
         combinations = list(itertools.combinations(position_possibilities['possibilities'], position_slots))
-        print(combinations)
+        position_combinations[position_possibilities['position']] = combinations
+
+    roster_combinations = []
+    point_combinations = []
+    # Nested for loops to collect all roster combinations
+    if True:
+        for qb in position_combinations["QB"]:
+            for rb in position_combinations["RB"]:
+                for wr in position_combinations["WR"]:
+                    for te in position_combinations["TE"]:
+                        for k in position_combinations["K"]:
+                            for defense in position_combinations["DEF"]:
+                                for wrt in position_combinations["W/R/T"]:
+                                    for flex in wrt: # check if any flex players are duplicating
+                                        if True: # no_dupe_checker(flex,rb,wr,te):
+                                            roster_combinations.append([qb, rb, wr, te, wrt, k, defense])
+                                            point_combinations.append(sum([player['points'] for position in roster_combinations[-1] for player in position])) # flattens combination list and extracts only points
+
+        print(manager['manager'], "\t", len(roster_combinations))
+        # s = pd.Series(point_combinations)
+        # print(s.describe())
+        arr = np.array(point_combinations)
+
+        bins = np.linspace(math.ceil(min(arr)),
+                           math.floor(max(arr)),
+                           20)  # fixed number of bins
+
+        plt.xlim([min(arr) - 5, max(arr) + 5])
+
+        plt.hist(arr, bins=bins, alpha=0.5)
+        plt.title(f'{manager["manager"]} - Week {week}')
+        plt.xlabel('Points')
+        plt.ylabel('Number of Roster Combinations')
+
+        plt.show()
 
 
-    # Edit lists of combinations
-        # Remove invalid combinations where players are in multiple slots (ex. WR and W/R/T)
-        # Remove duplicate combinations where players are swapped slots (ex. WR in one combi and W/R/T in another)
+        # league_combinations.append()
 
 
-# Calculate all possible scores
+        # [ QB(AB, AC), WR (11, 12), RB (ZY, ZX)...]
+        """
+        967680
+        882000
+        3427200
+        1814400
+        1209600
+        1234800"""
+stop = time.perf_counter()
 
+print("time", stop - start)
+print("Graphs and Statistics")
 # Create graphs
 # Histogram of possible scores
 # Percentile of chosen roster
-# Comparision in matchups
+# Comparison in matchups
 
     # "name": player.name.full,
     # "week": week_num,
